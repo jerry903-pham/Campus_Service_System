55fcd9709d72a18b03ccfe93dd2919272227c0c7
jerry903-pham <phamhoang.giabao2003@gmail.com>
2025-08-23 06:19:36 +0700

    Update Main.java

--- campus-app/src/main/java/com/greenwich/university/Main.java
+++ campus-app/src/main/java/com/greenwich/university/Main.java
@@ -1,17 +1,64 @@
 package com.greenwich.university;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int choice = -1;
+
+        while (choice != 7) {
+            System.out.println("\n=== Campus Service System Menu ===");
+            System.out.println("1. Course Registration");
+            System.out.println("2. Digital Library System");
+            System.out.println("3. File Tracker / Assignment Planner");
+            System.out.println("4. Room Booking System");
+            System.out.println("5. Campus Event Calendar");
+            System.out.println("6. Print Job Manager");
+            System.out.println("7. Exit");
+            System.out.print("Enter your choice: ");
+
+            try {
+                choice = Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a number between 1 and 7.");
+                continue;
+            }
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Launching Course Registration...");
+                    // TODO: call CourseRegistration module methods
+                    break;
+                case 2:
+                    System.out.println("Launching Digital Library System...");
+                    // TODO: call DigitalLibrarySystem methods
+                    break;
+                case 3:
+                    System.out.println("Launching File Tracker / Assignment Planner...");
+                    // TODO: call FileTracker methods
+                    break;
+                case 4:
+                    System.out.println("Launching Room Booking System...");
+                    // TODO: call RoomBookingSystem methods
+                    break;
+                case 5:
+                    System.out.println("Launching Campus Event Calendar...");
+                    // TODO: call CampusEventCalendar methods
+                    break;
+                case 6:
+                    System.out.println("Launching Print Job Manager...");
+                    // TODO: call PrintJobManager methods
+                    break;
+                case 7:
+                    System.out.println("Exiting. Goodbye!");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please enter a number between 1 and 7.");
+            }
         }
+
+        scanner.close();
     }
 }
